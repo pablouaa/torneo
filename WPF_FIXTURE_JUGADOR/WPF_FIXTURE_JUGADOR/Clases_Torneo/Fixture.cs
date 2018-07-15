93cdcf22a5e89675e4e5189a4e9994732abe7743
Koichi <arcana94hunter@gmail.com>
2018-07-15 14:41:25 -0400

    wpf y apis fixture/jugador(corregido)

---
+++ WPF_FIXTURE_JUGADOR/WPF_FIXTURE_JUGADOR/Clases_Torneo/Fixture.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Torneo
+{
+    public class Fixture
+    {
+        public dynamic id_partido { get; set; }
+        public dynamic torneo { get; set; }
+        public dynamic equipolocal { get; set; }
+        public dynamic equipovisitante { get; set; }
+        public dynamic fecha { get; set; }
+
+        public override string ToString()
+        {
+            return (this.torneo+" "+this.equipolocal+" vs "+equipovisitante);
+        }
+
+        public Fixture() { }
+        public Fixture(dynamic id_partido, dynamic torneo_id, dynamic equipo_local_id, dynamic equipo_visitante_id, dynamic fecha_nro)
+        {
+            this.id_partido = id_partido;
+            this.torneo = torneo_id;
+            this.equipolocal = equipo_local_id;
+            this.equipovisitante = equipo_visitante_id;
+            this.fecha = fecha_nro;
+        }
+
+
+        public static async Task<dynamic> ObtenerTodos()
+        {
+            string json = null;
+            dynamic datos = null;
+            //List<dynamic> lstpersonas = new List<dynamic>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:3000");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage respuesta = await client.GetAsync("/api/fixture");
+
+                if (respuesta.IsSuccessStatusCode)
+                {
+                    json = respuesta.Content.ReadAsStringAsync().Result.Replace("\\", "").Trim(new char[1] { '"' });
+                    //Console.WriteLine("Lo devuelto es " + json);
+                    datos = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                    //lstpersonas = await respuesta.Content.ReadAsAsync<List<dynamic>>();
+                    //lstpersonas.Add(json);
+                }
+            }
+            return datos;
+        }
+
+        public static async Task<dynamic> ObtenerEquipos()
+        {
+            string json = null;
+            dynamic equipo = null;
+
+            using (var team = new HttpClient())
+            {
+                team.BaseAddress = new Uri("http://localhost:3000");
+                team.DefaultRequestHeaders.Accept.Clear();
+                team.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage respuesta = await team.GetAsync("/api/equipos");
+
+                if (respuesta.IsSuccessStatusCode)
+                {
+                    json = respuesta.Content.ReadAsStringAsync().Result.Replace("\\", "").Trim(new char[1] { '"' });
+                    equipo = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                }
+            }
+            return equipo;
+        }
+    }
+}
---
+++ WPF_FIXTURE_JUGADOR/WPF_FIXTURE_JUGADOR/Clases_Torneo/Jugador.cs
@@ -0,0 +1,134 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Torneo
+{
+    public class Jugador
+    {
+
+        public int id_jugador { get; set; }
+        public string nro_documento { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public string url_imagen_perfil { get; set; }
+        public int total_tantos_convertidos { get; set; }
+        public string fecha_nacimiento { get; set; }
+
+        public Jugador() { }
+        public Jugador(int v1, string v2, string v3, string v4, string v5, int v6, string v7)
+        {
+            this.id_jugador = v1;
+            this.nro_documento = v2;
+            this.nombres = v3;
+            this.apellidos = v4;
+            this.fecha_nacimiento = v5;
+            this.total_tantos_convertidos = v6;
+            this.url_imagen_perfil = v7;
+        }
+
+        public static async Task<dynamic> getJugador(int id)
+        {
+            string json = null;
+            dynamic jugador = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:3000");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage respuesta = await client.GetAsync("/api/jugadores/" + id);
+
+                if (respuesta.IsSuccessStatusCode)
+                {
+                    json = respuesta.Content.ReadAsStringAsync().Result;
+                    jugador = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                }
+            }
+            return jugador;
+        }
+        public static async Task<dynamic> getJugadorDocumento(String doc)
+        {
+            string json = null;
+            dynamic jugador = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:3000");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage respuesta = await client.GetAsync("/api/jugadores/buscar/" + doc);
+
+                if (respuesta.IsSuccessStatusCode)
+                {
+                    json = respuesta.Content.ReadAsStringAsync().Result;
+                    jugador = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                }
+            }
+            return jugador;
+        }
+
+        public static async Task<dynamic> getJugadores()
+        {
+            string json = null;
+            dynamic jugadores = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:3000");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage respuesta = await client.GetAsync("/api/jugadores/");
+
+                if (respuesta.IsSuccessStatusCode)
+                {
+                    json = respuesta.Content.ReadAsStringAsync().Result;
+                    jugadores = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                }
+            }
+            return jugadores;
+        }
+
+        public static async Task<bool> postJugador(Jugador j)
+        {
+            
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:3000");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                JObject jugador = (JObject)JToken.FromObject(j);
+                HttpResponseMessage respuesta = await client.PostAsJsonAsync("/api/jugadores", jugador);
+                return respuesta.IsSuccessStatusCode;              
+            }
+        }
+
+        public static async Task<bool> putJugador(Jugador j)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:3000");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                JObject jugador = (JObject)JToken.FromObject(j);
+                HttpResponseMessage respuesta = await client.PutAsJsonAsync("/api/jugadores/"+j.id_jugador, jugador);
+                return respuesta.IsSuccessStatusCode;
+            }
+        }
+
+        public static async Task<bool> deleteJugador(Jugador j)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:3000");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage respuesta = await client.DeleteAsync("/api/jugadores/" + j.id_jugador);
+                return respuesta.IsSuccessStatusCode;
+            }
+        }
+    }
+}
